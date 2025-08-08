e81c710e9b9f99d2d6ad3b71a0fa4ccb5f415c50
Mikiyas <mikiyealem@gmail.com>
2025-08-08 15:13:14 +0300

    fix / fix in the style of the login page

--- src/app/landing_page/landing_page.tsx
+++ src/app/landing_page/landing_page.tsx
@@ -27,7 +27,7 @@ export default function LandingPage() {
               fast-tracked to a software engineering career at the world’s
               leading tech companies.
             </p>
-            <button className="px-6 py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
+            <button className="px-6 py-3 bg-[#4F46E5] text-white rounded-md hover:bg-[#4338CA] transition">
               Start Your Application
             </button>
           </div>
@@ -52,91 +52,92 @@ export default function LandingPage() {
         </section>
 
         {/* Journey Section */}
-        <section className="py-12 px-6 md:px-20 bg-white">
+        <section className="py-12 px-5 md:px-20 bg-white">
           <h2 className="text-3xl font-bold text-center">
             Your Journey to Silicon Valley{" "}
           </h2>
           <p className="text-center text-gray-600 mb-5">
             A proven path from learning to leadership
           </p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+            <div className="bg-white p-5 rounded-lg shadow hover:shadow-md transition flex items-start">
+              <Image
+                src="/images/phase1.svg"
+                alt="Screening Icon"
+                width={48}
+                height={48}
+                className="mr-1.5"
+              />
+              <div>
+                <h3 className="text-[16.5px] font-semibold mb-4">
+                  Phase 1: Foundations
+                </h3>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-            {/* Card 1: Screening */}
-            <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition">
-              <div className="flex items-center mb-2">
-                <Image
-                  src="/images/phase1.svg"
-                  alt="Screening Icon"
-                  width={24}
-                  height={24}
-                  className="mr-2"
-                />
-                <h3 className="text-xl font-semibold">Phase 1: Foundations</h3>
+                <p className="text-gray-600 text-[15px]">
+                  Master data structures, algorithms,and problem-solving
+                  techniques in intensive 3-month bootcamp.
+                </p>
               </div>
-              <p className="text-gray-600">
-                Master data structures, algorithms, and problem-solving
-                techniques in an intensive 3-month bootcamp.
-              </p>
             </div>
-
             {/* Card 2: Bootcamp */}
-            <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition">
-              <div className="flex items-center mb-2">
-                <Image
-                  src="/images/phase2.svg"
-                  alt="Bootcamp Icon"
-                  width={24}
-                  height={24}
-                  className="mr-2"
-                />
-                <h3 className="text-xl font-semibold">
+            <div className="bg-white p-5 rounded-lg shadow hover:shadow-md transition flex items-start">
+              <Image
+                src="/images/phase2.svg"
+                alt="Bootcamp Icon"
+                width={48}
+                height={48}
+                className="mr-2"
+              />
+              <div>
+                <h3 className="text-[16.5px] font-semibold mb-2">
                   Phase 2: Real-world Projects
                 </h3>
+
+                <p className="text-gray-600 text-[15px]">
+                  Apply your skills to build complex projects, collaborate in
+                  teams, and prepare for technical interviews.
+                </p>
               </div>
-              <p className="text-gray-600">
-                Apply your skills to build complex projects, collaborate in
-                teams, and prepare for technical interviews.
-              </p>
             </div>
 
             {/* Card 3: Mentorship */}
-            <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition">
-              <div className="flex items-center mb-2">
-                <Image
-                  src="/images/phase3.svg"
-                  alt="Mentorship Icon"
-                  width={24}
-                  height={24}
-                  className="mr-2"
-                />
-                <h3 className="text-xl font-semibold">
+            <div className="bg-white p-5 rounded-lg shadow hover:shadow-md transition flex items-start">
+              <Image
+                src="/images/phase3.svg"
+                alt="Mentorship Icon"
+                width={48}
+                height={48}
+                className="mr-4"
+              />
+              <div>
+                <h3 className="text-[16.5px] font-semibold mb-2">
                   Phase 3: Internship Placement
                 </h3>
+                <p className="text-gray-600 text-[15px]">
+                  We help you secure internships at top global tech companies to
+                  gain invaluable experience.
+                </p>
               </div>
-              <p className="text-gray-600">
-                We help you secure internships at top global tech companies to
-                gain invaluable experience.
-              </p>
             </div>
 
             {/* Card 4: Employment */}
-            <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition">
-              <div className="flex items-center mb-2">
-                <Image
-                  src="/images/phase4.svg"
-                  alt="Employment Icon"
-                  width={24}
-                  height={24}
-                  className="mr-2"
-                />
-                <h3 className="text-xl font-semibold">
+            <div className="bg-white p-5 rounded-lg shadow hover:shadow-md transition flex items-start">
+              <Image
+                src="/images/phase4.svg"
+                alt="Employment Icon"
+                width={48}
+                height={48}
+                className="mr-4"
+              />
+              <div>
+                <h3 className="text-[16.5px] font-semibold mb-2">
                   Phase 4: Full-Time Conversion
                 </h3>
+                <p className="text-gray-600 text-[15px]">
+                  Excel in your internship and convert it into a full-time
+                  offer, launching your global career.
+                </p>
               </div>
-              <p className="text-gray-600">
-                Excel in your internship and convert it into a full-time offer,
-                launching your global career.
-              </p>
             </div>
           </div>
         </section>
@@ -145,7 +146,7 @@ export default function LandingPage() {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center mx-50 my-20">
             {/* Left Text Content */}
             <div>
-              <h2 className="text-3xl md:text-4xl font-bold mb-4">
+              <h2 className="text-[28px] md:text-4xl font-bold mb-4">
                 Built by Engineers, for Engineers
               </h2>
               <p className="text-gray-600 mb-6">
@@ -170,100 +171,119 @@ export default function LandingPage() {
         </section>
 
         <section className="py-16 px-6 md:px-20 bg-gray-50">
-  <h2 className="text-3xl font-bold text-center mb-10">Meet Our Alumni</h2>
+          <h2 className="text-[28px] font-bold text-center mb-10">
+            Hear From Our Alumni
+          </h2>
 
-  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-    {/* Alumni Card Example */}
-    <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition flex flex-col justify-between h-full">
-      {/* Testimonial Text */}
-      <p className="text-gray-600 mb-6">
-        "A2SV completely changed the trajectory of my career. The training is intense, but of the community and the opportunities are unparalleled. I'm now at my dream company, and I owe it all to A2SV."
-      </p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition flex flex-col justify-between h-full">
+              <p className="text-gray-600 mb-6">
+                "A2SV completely changed the trajectory of my career. The
+                training is intense, but of the community and the opportunities
+                are unparalleled. I'm now at my dream company, and I owe it all
+                to A2SV."
+              </p>
+              <div className="flex items-center ">
+                <Image
+                  src="/images/mosh.png"
+                  alt="Sara Kebede"
+                  width={50}
+                  height={50}
+                  className="rounded-full object-cover"
+                />
 
-      {/* Bottom Row: Image & Info */}
-      <div className="flex items-center justify-between">
-        {/* Profile Image */}
-        <Image
-          src="/images/mosh.png"
-          alt="Sara Kebede"
-          width={50}
-          height={50}
-          className="rounded-full object-cover"
-        />
+                {/* Name & Role */}
+                <div className="ml-3">
+                  <h3 className="font-semibold text-lg">Abel Tadesse</h3>
+                  <span className="text-sm text-gray-500">
+                    Software Engineer @ Google
+                  </span>
+                </div>
+              </div>
+            </div>
 
-        {/* Name & Role */}
-        <div className="text-right">
-          <h3 className="font-semibold text-lg">Abel Tadesse</h3>
-          <span className="text-sm text-gray-500">Software Engineer @ Google</span>
-        </div>
-      </div>
-    </div>
+            {/* Repeat Cards for Other Alumni... */}
 
-    {/* Repeat Cards for Other Alumni... */}
+            <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition flex flex-col justify-between h-full">
+              {/* Testimonial Text */}
+              <p className="text-gray-600 mb-6">
+                "The problem-solving skills I learned at A2SV are invaluable.
+                The mentors push you to be your best and you're surrounded by
+                people who are just as passionate as you are."
+              </p>
 
-     <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition flex flex-col justify-between h-full">
-      {/* Testimonial Text */}
-      <p className="text-gray-600 mb-6">
-        "The problem-solving skills I learned at A2SV are invaluable. The mentors push you to be your best and you're surrounded by people who are just as passionate as you are."
-      </p>
+              {/* Bottom Row: Image & Info */}
+              <div className="flex items-center">
+                {/* Profile Image */}
+                <Image
+                  src="/images/angela.svg"
+                  alt="Sara Kebede"
+                  width={50}
+                  height={50}
+                  className="rounded-full object-cover"
+                />
 
-      {/* Bottom Row: Image & Info */}
-      <div className="flex items-center justify-between">
-        {/* Profile Image */}
-        <Image
-          src="/images/angela.svg"
-          alt="Sara Kebede"
-          width={50}
-          height={50}
-          className="rounded-full object-cover"
-        />
+                {/* Name & Role */}
+                <div className="ml-3">
+                  <h3 className="font-semibold text-lg">Bethlehem Tadesse</h3>
+                  <span className="text-sm text-gray-500">
+                    Software Engineer, Amazon
+                  </span>
+                </div>
+              </div>
+            </div>
 
-        {/* Name & Role */}
-        <div className="text-right">
-          <h3 className="font-semibold text-lg">Bethlehem Tadesse</h3>
-          <span className="text-sm text-gray-500">Software Engineer, Amazon</span>
-        </div>
-      </div>
-    </div>
+            <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition flex flex-col justify-between h-full">
+              {/* Testimonial Text */}
+              <p className="text-gray-600 mb-6">
+                "A2SV is more than a bootcamp. It's a family that supports you
+                long after you've graduated. The network you build here is for
+                life."
+              </p>
 
-     <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition flex flex-col justify-between h-full">
-      {/* Testimonial Text */}
-      <p className="text-gray-600 mb-6">
-        "A2SV is more than a bootcamp. It's a family that supports you long after you've graduated. The network you build here is for life."
-      </p>
+              {/* Bottom Row: Image & Info */}
+              <div className="flex items-center">
+                {/* Profile Image */}
+                <Image
+                  src="/images/angela2.svg"
+                  alt="Sara Kebede"
+                  width={50}
+                  height={50}
+                  className="rounded-full object-cover"
+                />
 
-      {/* Bottom Row: Image & Info */}
-      <div className="flex items-center justify-between">
-        {/* Profile Image */}
-        <Image
-          src="/images/angela2.svg"
-          alt="Sara Kebede"
-          width={50}
-          height={50}
-          className="rounded-full object-cover"
-        />
+                {/* Name & Role */}
+                <div className="ml-3">
+                  <h3 className="font-semibold text-lg">Caleb Alemayehu</h3>
+                  <span className="text-sm text-gray-500">
+                    Software Engineer, Palantir
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
 
-        {/* Name & Role */}
-        <div className="text-right">
-          <h3 className="font-semibold text-lg">Caleb Alemayehu</h3>
-          <span className="text-sm text-gray-500">Software Engineer, Palantir</span>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-<section className="bg-[#4338CA] h-64">
-  <div className="p-10">
-  <h3 className="font-semibold text-white text-lg text-center pt-7 ">Ready to Change your life</h3>
-  <p className="text-sm text-white text-center">The next application cycle now open. Take the first step towards your dream career</p>
-  <div className="flex justify-center mt-6">
-    <Link href="#" className="px-6 py-3 bg-white text-blue-500 rounded-md  transition">
-      Apply Now
-    </Link> 
-    </div>
-</div>
-</section>
+        <section className="bg-[#4338CA] h-64">
+          <div className="p-10">
+            <h3 className="font-semibold text-white text-[22px] text-center pt-7 ">
+              Ready to Change your life quickly?
+            </h3>
+            <p className="text-sm pt-2 text-center text-[#C7D2FE]">
+              The next application cycle now open. Take the first step towards
+              <br />
+              your dream career
+            </p>
+            <div className="flex justify-center mt-5">
+              <Link
+                href="#"
+                className="px-5 py-2 bg-white text-[#4F46E5] rounded-md  transition hover:bg-gray-300"
+              >
+                Apply Now
+              </Link>
+            </div>
+          </div>
+        </section>
       </main>
       <Footer />
     </div>
