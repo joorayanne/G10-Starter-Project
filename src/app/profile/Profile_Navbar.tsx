fbbf95a28e5b79d1bf80600167088a170be62263
Adey <adeyebuy2@gmail.com>
2025-08-09 22:50:48 +0300

    modified image path

--- src/app/profile/Profile_Navbar.tsx
+++ src/app/profile/Profile_Navbar.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
 import { signOut, useSession } from "next-auth/react";
-import logo from "../../../public/images/logo.png";
 
 const navLinks = [{ name: "Dashboard", href: "" }];
 
@@ -15,7 +14,7 @@ export default function ProfileNavbar() {
   return (
     <nav className="flex items-center justify-between w-full px-4 sm:px-8 py-4 bg-white border-b border-gray-200">
       <Link href="/profile" className="flex items-center space-x-2">
-        <Image src={logo} width={100} height={20} className="sm:w-[120px] sm:h-[24px]" alt="A2SV Logo" />
+        <Image src="/images/logo.png" width={100} height={20} className="sm:w-[120px] sm:h-[24px]" alt="A2SV Logo" />
       </Link>
 
       <div className="hidden sm:flex items-center space-x-6">
